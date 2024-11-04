562bea57719066a9c94431650ad57fe8db130f61
Chandler Kuhn <ctkuhn@calpoly.edu>
2024-11-04 13:28:39 -0800

    code for password

--- Admission.py
+++ Admission.py
@@ -7,10 +7,6 @@ import pickle
 import warnings
 warnings.filterwarnings('ignore')
 
-password_guess = st.text_input("What is the Password?")
-if password_guess != st.secrets["ckuhnadmissions"]:
- st.stop()
-
 # Set up the app title and image
 st.title('Graduate Admission Predictor 🌟')
 st.image('admission.jpg', use_column_width = True, 
@@ -100,4 +96,8 @@ with tab4:
     st.write("### Coverage Plot")
     st.image('coverage.svg')
     st.caption("Range of predictions with confidence intervals.")
+
+password_guess = st.text_input("What is the Password?")
+if password_guess != st.secrets["ckuhnadmissions"]:
+ st.stop()
 
